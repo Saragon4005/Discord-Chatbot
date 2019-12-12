09e59444b3d0faf08a02696a64e346dd8f44a567
Saragon4005 <domikakeszei@gmail.com>
2019-12-12 12:24:01 -0800

    correct sytax

--- Main.py
+++ Main.py
@@ -10,6 +10,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(client.user + "has connected to Discord!')
 
 client.run(token)
