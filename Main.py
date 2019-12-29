c42acc2276347ef28e2591580340c9a0b324903b
Saragon4005 <domikakeszei@gmail.com>
2019-12-29 11:21:59 -0800

    changed print f string to normal print

--- Main.py
+++ Main.py
@@ -8,8 +8,9 @@ token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(client.user + 'has connected to Discord!')
 
 client.run(token)
