9e83d505194afd8c01fff804e4f0737a0155fd37
Saragon4005 <domikakeszei@gmail.com>
2019-12-29 13:29:59 -0800

    Update to python3.8

--- Main.py
+++ Main.py
@@ -11,6 +11,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(client.user.name + ' has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
 client.run(token)
