eb4b17f2794534e56291b712fe46f2241fe050eb
Saragon4005 <domikakeszei@gmail.com>
2019-12-29 15:19:45 -0800

    Gild connection

--- Main.py
+++ Main.py
@@ -4,13 +4,21 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(token)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
