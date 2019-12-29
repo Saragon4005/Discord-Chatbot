2a26fdd9888e598b0d45d8f87d9e086f0c47887e
Saragon4005 <domikakeszei@gmail.com>
2019-12-29 15:51:13 -0800

    Error handleing

--- Main.py
+++ Main.py
@@ -20,5 +20,26 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+"""
+async def on_member_join(member):
+    await message.channel.send(f'Welcome {member.name}!')
+
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+"""
+
+
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
 client.run(TOKEN)
