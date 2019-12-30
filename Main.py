80e04863f54cdc3560684e4a9010791f9554edae
Saragon4005 <domikakeszei@gmail.com>
2019-12-30 08:12:29 -0800

    add test command

--- Main.py
+++ Main.py
@@ -24,7 +24,6 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
 
 
-"""
 async def on_member_join(member):
     await message.channel.send(f'Welcome {member.name}!')
 
@@ -32,7 +31,8 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-"""
+    if message.content.startswith('!test'):
+        await message.channel.send('works!')
 
 
 async def on_error(event, *args, **kwargs):
