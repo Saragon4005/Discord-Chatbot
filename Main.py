62719cfb4e79573909f31edd61964a1c195edaf9
Saragon4005 <domikakeszei@gmail.com>
2020-01-08 10:49:24 -0800

    Add moirail traking with presistent saving

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import Logger
+import json
 
 
 load_dotenv()
@@ -16,52 +17,63 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix='!')
 
-
-@bot.command(name="Test", help="Responds with 'works!'")
-async def test(ctx):
-    await ctx.send("works!")
-
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+Moirail = {}
+with open('Moirail.json', 'r') as fp:
+    Moirail = json.load(fp)
 
 
-@client.event
-async def on_connect():
-    print("Connected!")
+def save():
+    with open('Moirail.json', 'w') as fp:
+        json.dump(Moirail, fp)
 
 
-@client.event
-async def on_member_join(member):
-    channel = client.get_channel(660987946085646367)
-    await channel.send(f'Welcome {member.name}!')
+try:
+    @bot.command(name="Test", help="Responds with 'works!'")
+    async def test(ctx):
+        await ctx.send("works!")
 
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('!test'):
-        await message.channel.send('works!')
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
 
+    @client.event
+    async def on_connect():
+        print("Connected!")
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+    @client.event
+    async def on_member_join(member):
+        channel = client.get_channel(660987946085646367)
+        await channel.send(f'Welcome {member.name}!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        if message.content.startswith('!test'):
+            await message.channel.send('works!')
+        if message.content.incudes('<>'):
+            Moirail[message.author] += 1
+
+    @client.event
+    async def on_error(event, *args, **kwargs):
+        with open('err.log', 'a') as f:
+            if event == 'on_message':
+                f.write(f'Unhandled message: {args[0]}\n')
+            else:
+                raise
+except:
+    save()
+    raise
+
 
 client.run(TOKEN)
 bot.run(TOKEN)
