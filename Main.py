810007f946f6a181c806c66743b3e71f1569eda8
Saragon4005 <domikakeszei@gmail.com>
2020-03-18 12:54:28 -0700

    remove testing

--- Main.py
+++ Main.py
@@ -42,7 +42,7 @@ try:
     @bot.command(name="test", help="Responds with 'works!'")
     async def test(ctx):
         print('test triggered!')
-        await ctx.send("works! `1`")
+        await ctx.send("works!")
 
     @bot.command(name="Moirail", help="Shows moirail counter for user",
                  aliases=["m"])
