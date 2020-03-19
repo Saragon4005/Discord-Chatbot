b20a45b5f56aebea350991e6668d7c9ec68aa12a
Saragon4005 <domikakeszei@gmail.com>
2020-03-19 09:48:11 -0700

    Add basic SQL Database capabilities

---
+++ Database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+SQL = sqlite3.connect('users.db')
+c = SQL.cursor()
+
+
+def CreateDB():
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS Users'
+        '(id int NOT NULL PRIMARY KEY, Moirail int)')
+
+
+def QueryID(condition):
+    id = [condition]
+    c.execute(''' SELECT Moirail FROM Users WHERE id = ? ''', id)
+    return(c.fetchone())
+
+
+def update(Set: str, Condition: str):
+    # This should not be exposed to the user
+    c.execute(f'''UPDATE Users
+                 Set {Set}
+                 Where {Condition}''')
+
+
+CreateDB()
