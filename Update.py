2a28cf6a13534ec20c4d71800082aa749a85046f
Saragon4005 <domikakeszei@gmail.com>
2020-04-12 14:38:45 -0700

    make updateing process more robust

--- Update.py
+++ Update.py
@@ -1,14 +1,20 @@
-import sqlite3
 from Database import c, SQL
 # from subprocess import call
 # call()
 
-for col in ["Seen", "LastMessage"]:
-    try:
-        c.execute(f'''ALTER TABLE Users
-        ADD {col} TIMESTAMP;''')
-    except sqlite3.OperationalError:
-        pass
+c.executescript("""
+BEGIN TRANSACTION;
+ALTER TABLE Users RENAME TO _Users_old;
+
+CREATE TABLE Users
+(id int NOT NULL PRIMARY KEY, Moirail int DEFAULT 0,
+Seen TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+LastMessage TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO Users (id, Moirail)
+SELECT id, Moirail
+FROM _Users_old;
+COMMIT;""")
 
 
 SQL.commit()
